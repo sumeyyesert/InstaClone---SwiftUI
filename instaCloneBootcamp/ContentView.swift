a67cca320811c314cc240f66c029c9fbf5cea57b
sumeyyesert <sumeyeesert@gmail.com>
2025-03-20 22:23:38 +0300

    Initial Commit

---
+++ instaCloneBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  instaCloneBootcamp
+//
+//  Created by Sümeyye Sert on 20.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ instaCloneBootcamp/instaCloneBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaCloneBootcampApp.swift
+//  instaCloneBootcamp
+//
+//  Created by Sümeyye Sert on 20.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct instaCloneBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
