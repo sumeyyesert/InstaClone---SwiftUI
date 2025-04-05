61f34e07b620dc509438b831f8137d64bfb0ee59
sumeyyesert <sumeyeesert@gmail.com>
2025-04-05 05:13:16 +0300

    UploadPostViewModel added.

---
+++ instaCloneBootcamp/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,67 @@
+//
+//  UploadPostView.swift
+//  instaCloneBootcamp
+//
+//  Created by Sümeyye Sert on 5.04.2025.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct UploadPostView: View {
+    @State private var caption = ""
+    @State private var imagePickerPresented = false
+    @StateObject var viewModel = UploadPostViewModel()
+    @Binding var tabIndex: Int
+    
+    var body: some View {
+        
+        VStack {
+            // Action toolbar
+            HStack {
+                Button{
+                    caption = ""
+                    viewModel.selectedImage = nil
+                    viewModel.postImage = nil
+                    tabIndex = 0
+                } label: {
+                    Text("Cancel")
+                }
+                Spacer()
+                Text("New Post")
+                    .fontWeight(.semibold)
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text("Upload")
+                        .fontWeight(.semibold)
+                }
+                
+            }
+            .padding(.horizontal)
+            
+            // Post ve açıklaması
+            HStack {
+                if let image = viewModel.postImage {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                }
+                
+                TextField("Enter your caption...", text: $caption, axis: .vertical)
+            }
+            Spacer()
+        }
+        .onAppear() {
+            imagePickerPresented.toggle()
+        }
+        .photosPicker(isPresented: $imagePickerPresented, selection: $viewModel.selectedImage)
+    }
+}
+
+#Preview {
+    UploadPostView(tabIndex: .constant(0))
+}
---
+++ instaCloneBootcamp/Core/UploadPost/UploadPostViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  UploadPostViewModel.swift
+//  instaCloneBootcamp
+//
+//  Created by Sümeyye Sert on 5.04.2025.
+//
+
+import Foundation
+import PhotosUI
+import SwiftUI
+
+class UploadPostViewModel: ObservableObject{
+    
+    @Published var selectedImage: PhotosPickerItem? {
+        didSet {
+            Task {
+                await loadImage(fromItem: selectedImage)
+            }
+        }
+    }
+    
+    @Published var postImage: Image?
+
+    func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item = item else { return }
+        
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: data) else { return }
+
+        self.postImage = Image(uiImage: uiImage)
+    }
+
+}
